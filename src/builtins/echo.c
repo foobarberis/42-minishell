c56a0a84613cf10a7587ce035f4b9c1800e79d5c
foobarberis <foobarberis@gmail.com>
2023-05-12 11:35:04 +0000

    Implement echo.

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -16,8 +16,6 @@ static bool contains_valid_option(char *s)
 }
 
 /* https://www.gnu.org/software/bash/manual/html_node/Bash-Builtins.html#index-echo */
-/* WARNING: echo -n -n -> prints nothing */
-/* WARNING: If argv[i] is not a valid option, subsequent options are printed normally. */
 int blt_echo(char **argv)
 {
 	size_t i;
@@ -42,27 +40,3 @@ int blt_echo(char **argv)
 		printf("\n");
 	return (0);
 }
-
-/* int blt_echo(int argc, char **argv)
-{
-	size_t i;
-	bool   newline;
-
-	if (!argv)
-		return (0);
-	i = 1;
-	newline = contains_valid_option(argv[1]);
-	if (newline)
-		i++;
-	while (argc > 0)
-	{
-		printf("%s", argv[0]);
-		argc--;
-		argv++;
-		if (argc > 0)
-			putchar(' ');
-	}
-	if (newline)
-		f_printf("\n");
-	return (0);
-} */
