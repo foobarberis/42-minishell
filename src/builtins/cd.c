0ef1cf8f35315a44d9804a59a6e83ec2bd6712e4
foobarberis <foobarberis@gmail.com>
2023-05-19 12:52:06 +0000

    Fix cd behavior and add proper error messages.

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,50 +1,39 @@
 #include "minishell.h"
 
-/* FIXME: Secure this */
 static int update_pwd(t_glb *glb)
 {
 	char *tmp;
-	char *pwd;
 	char buf[PATH_MAX];
 
 	tmp = env_getenv(glb->env, "PWD");
-	pwd = f_strjoin("OLDPWD=", tmp);
-	glb->env = env_key_add(glb->env, pwd);
-	free(pwd);
+	glb->env = env_key_add(glb->env, f_strjoin("OLDPWD=", tmp));
 	if (!glb->env)
 		panic(glb, CODE_MALLOC, NULL);
-	pwd = f_strjoin("PWD=", getcwd(buf, PATH_MAX));
-	glb->env = env_key_add(glb->env, pwd);
-		free(pwd);
+	glb->env = env_key_add(glb->env, f_strjoin("PWD=", getcwd(buf, PATH_MAX)));
 	if (!glb->env)
-			panic(glb, CODE_MALLOC, NULL);
-
+		panic(glb, CODE_MALLOC, NULL);
 	return (0);
 }
 
 /* https://man7.org/linux/man-pages/man1/cd.1p.html */
 int blt_cd(t_glb *glb, int argc, char **argv)
 {
-	int   ret;
 	char *path;
 
 	if (argc > 2)
-		return (f_dprintf(STDERR_FILENO, "minishell: cd: too many "
-		                                 "arguments\n"));
-	if (!argv[1])
+		return (f_dprintf(STDERR_FILENO, "minishell: cd: too many arguments\n"));
+	else if (!argv[1])
 	{
 		path = env_getenv(glb->env, "HOME");
 		if (!path || !*path)
 			return (f_dprintf(STDERR_FILENO, "minishell: cd: HOME not set\n"));
-		ret = chdir(path);
-		if (ret)
+		if (chdir(path))
 			return (perror("minishell: cd: "), 1);
 	}
 	else if (!argv[1][0])
 		return (0);
-	ret = chdir(argv[1]);
-	if (ret)
-		return (perror("minishell: cd: "), 1);
+	else if (chdir(argv[1]))
+		return (f_dprintf(STDERR_FILENO, "minishell: cd: %s: ", argv[1]), perror(""), 1);
 	update_pwd(glb);
 	return (0);
 }
